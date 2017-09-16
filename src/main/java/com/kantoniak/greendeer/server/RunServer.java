c04c03bee6faeda918d76d159dfea52ffe1dfac2
Krzysztof Antoniak <krzysztof@antoniak.in>
2017-09-16 13:11:05 +0200

    Create project

---
+++ src/main/java/com/kantoniak/greendeer/server/RunServer.java
@@ -0,0 +1,72 @@
+package com.kantoniak.greendeer.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import java.io.IOException;
+import java.util.logging.Logger;
+import com.kantoniak.greendeer.proto.RunServiceGrpc;
+import com.kantoniak.greendeer.proto.GetListResponse;
+import com.kantoniak.greendeer.proto.GetListRequest;
+
+/**
+ * Server that manages startup/shutdown of a {@code Greeter} server.
+ */
+public class RunServer {
+  private static final Logger logger = Logger.getLogger(RunServer.class.getName());
+
+  private Server server;
+
+  private void start() throws IOException {
+    /* The port on which the server should run */
+    int port = 50051;
+    server = ServerBuilder.forPort(port)
+        .addService(new RunServiceImpl())
+        .build()
+        .start();
+    logger.info("Server started, listening on " + port);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+        System.err.println("*** shutting down gRPC server since JVM is shutting down");
+        RunServer.this.stop();
+        System.err.println("*** server shut down");
+      }
+    });
+  }
+
+  private void stop() {
+    if (server != null) {
+      server.shutdown();
+    }
+  }
+
+  /**
+   * Await termination on the main thread since the grpc library uses daemon threads.
+   */
+  private void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
+    }
+  }
+
+  /**
+   * Main launches the server from the command line.
+   */
+  public static void main(String[] args) throws IOException, InterruptedException {
+    final RunServer server = new RunServer();
+    server.start();
+    server.blockUntilShutdown();
+  }
+
+  static class RunServiceImpl extends RunServiceGrpc.RunServiceImplBase {
+
+    @Override
+    public void getList(GetListRequest req, StreamObserver<GetListResponse> responseObserver) {
+      GetListResponse reply = GetListResponse.newBuilder().build();
+      responseObserver.onNext(reply);
+      responseObserver.onCompleted();
+    }
+  }
+}
